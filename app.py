9ead9c9b697f4128c8c95bb963be240e5953b2e8
Jaqueline-santoos <87211727+jaque-santos@users.noreply.github.com>
2021-07-11 11:50:08 -0300

    sei la

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import flask
 TEMPLATE = './template'
 STATIC = './static'
 app = flask(__name__, template_folder = TEMPLATE, static_folder = STATIC)
-@app.router('/')
+@app.route('/')
 def index():
     return 'Ola mundo'
 app.run(host = "0.0.0.0", port = 5000)
