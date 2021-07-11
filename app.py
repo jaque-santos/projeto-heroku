0fcc1d7cfdea1fcec5dffbbfafcc8bd04a1d3c33
Jaqueline-santoos <87211727+jaque-santos@users.noreply.github.com>
2021-07-11 12:14:05 -0300

    jaque funcionou

--- app.py
+++ app.py
@@ -1,2 +1,14 @@
+from typing import Text
 from flask import Flask
 
+TEMPLATES = './templates'
+STATIC = './static'
+
+app = Flask(__name__, template_folder=TEMPLATES, static_folder=STATIC)
+
+@app.route('/')
+def helloworld():
+    return 'Olá, mundo!'
+
+app.run(host='0.0.0.0', port=5000)
+    
