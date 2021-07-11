395acc398af5e4447d2ff04b974857e2c5cacff0
Jaqueline-santoos <87211727+jaque-santos@users.noreply.github.com>
2021-07-11 12:36:26 -0300

    sla

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from typing import Text
-from flask import Flask
+from flask import Flask, render_template
 
 TEMPLATES = './templates'
 STATIC = './static'
@@ -9,6 +9,10 @@ app = Flask(__name__, template_folder=TEMPLATES, static_folder=STATIC)
 @app.route('/')
 def helloworld():
     return 'Olá, mundo!'
+
+@app.route('/home')
+def home():
+    return render_template('home.html')
 
 app.run(host='0.0.0.0', port=5000)
     
