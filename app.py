151f6a1e8bc1fac1acb1b96c86fbccd509c91e40
Jaqueline-santoos <87211727+jaque-santos@users.noreply.github.com>
2021-07-11 21:26:51 -0300

    envio index

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
-from typing import Text
 from flask import Flask, render_template
-
 TEMPLATES = './templates'
 STATIC = './static'
 
@@ -8,11 +6,11 @@ app = Flask(__name__, template_folder=TEMPLATES, static_folder=STATIC)
 
 @app.route('/')
 def helloworld():
-    return 'Olá, mundo!'
+    return 'Olá mundo!'
 
-@app.route('/home')
-def home():
-    return render_template('home.html')
-
-app.run(host='0.0.0.0', port=5000)
+@app.route('/index')
+def index():
+    nome= 'Jaqueline'
+    return render_template('index.html', nome=nome)
+app.run(host='0.0.0.0', port=5000) 
     
