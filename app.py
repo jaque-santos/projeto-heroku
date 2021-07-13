edc6ef72a07f5f8029a44f9cadc849a90def8d16
Jaqueline-santoos <87211727+jaque-santos@users.noreply.github.com>
2021-07-12 23:06:14 -0300

    Atualização

--- app.py
+++ app.py
@@ -16,5 +16,6 @@ def index():
     nome= 'Jaqueline'
     Lista=['https://www.youtube.com/embed/3fP541Qhfd0','https://www.youtube.com/embed/CTIs_RSPr84']
     return render_template('index.html', nome=nome, Lista=Lista, dataAtual = data)
+
 #app.run(host='0.0.0.0', port=5000) 
     
